0c03c53991049d89bc13ae4abb33ffc667c2e260
lephleg <lephleg@gmail.com>
2020-12-04 13:26:46 +0200

    feat: add dummy pages for institution listing and document validation

---
+++ src/components/ValidatePage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+    title: {
+        textAlign: "center"
+    }
+});
+
+export default function ValidatePage(props) {
+    const classes = useStyles(props);
+
+    return (
+        <section>
+            <Container maxWidth="md" className={classes.title}>
+                <Typography variant="h3" component="h2">Validate a document</Typography>
+            </Container>
+        </section>
+    );
+}
