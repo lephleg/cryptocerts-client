34b8b8d118aa6319a295c1a44f1552a072fafb47
lephleg <lephleg@gmail.com>
2020-12-11 11:31:58 +0200

    feat: add a draft event listener for institutions

--- src/context/ConnectionProvider.js
+++ src/context/ConnectionProvider.js
@@ -4,6 +4,9 @@ import Web3Utils from '../utils/web3-utils';
 import { NotificationsContext } from './NotificationsProvider';
 import { useDispatch } from 'react-redux';
 import { setConnected as setConnectedAction, setActiveAccount, setWeb3Capable } from '../features/connection/connectionSlice';
+import { abi as CryptoCertsAbi } from '../contracts/CryptoCerts.json';
+import { CRYPTOCERTS_CONTRACT_ADDRESS } from '../config';
+import { fetchInstitutions } from '../features/institutions/institutionsSlice';
 
 export const ConnectionContext = React.createContext({
     connected: false,
@@ -26,6 +29,14 @@ export default function ConnectionProvider(props) {
         if (ready) {
             // needed in order to hide the relevant MetaMask console warning
             window.ethereum.autoRefreshOnNetworkChange = false;
+            // subscribe to blockchain events
+            // TODO: find a more clean way to import web3 + contract
+            let web3 = new Web3(window.ethereum);
+            let contract = new web3.eth.Contract(CryptoCertsAbi, CRYPTOCERTS_CONTRACT_ADDRESS);
+            contract.events.InstitutionCreated({}, (error, event) => {
+                console.log('New institution created!');
+                dispatch(fetchInstitutions());
+            });
         }
         setReadyForWeb3(ready);
         dispatch(setWeb3Capable(ready));
